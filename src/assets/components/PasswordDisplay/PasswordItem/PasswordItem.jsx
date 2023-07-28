b1c0a038d7dcf47f04310374f85f5250bd9214f5
Dra <nd.mirica@gmail.com>
2023-07-28 14:58:47 +0300

    now passwords are being displayed

--- src/assets/components/PasswordDisplay/PasswordItem/PasswordItem.jsx
+++ src/assets/components/PasswordDisplay/PasswordItem/PasswordItem.jsx
@@ -1,4 +1,24 @@
 export const PasswordItem = ({ entry }) => {
   const { id, email, imgsrc, site, user, pass } = entry;
-  return <article id={id}></article>;
+  return (
+    <article id={id}>
+      <div className="identifier">
+        <img src={imgsrc} alt="" />
+        <p>
+          Usage :
+          <input type="text" defaultValue={site} />
+        </p>
+      </div>
+      <div className="data-container">
+        <p>
+          User :
+          <input type="text" defaultValue={user} />
+        </p>
+        <p>
+          Password :
+          <input type="text" defaultValue={pass} />
+        </p>
+      </div>
+    </article>
+  );
 };
