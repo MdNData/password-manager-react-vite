42452854fc7476122ae1d8f03568728684e5534a
Dra <nd.mirica@gmail.com>
2023-07-29 13:08:12 +0300

    close and open password element

--- src/assets/components/PasswordDisplay/PasswordItem/PasswordItem.jsx
+++ src/assets/components/PasswordDisplay/PasswordItem/PasswordItem.jsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
+
 export const PasswordItem = ({ entry }) => {
   const { id, email, imgsrc, site, user, pass } = entry;
+  const [displayIt, setDisplayIt] = useState(false);
   return (
     <article id={id}>
       <div className="identifier">
@@ -8,17 +11,33 @@ export const PasswordItem = ({ entry }) => {
           Usage :
           <input type="text" defaultValue={site} />
         </p>
+        <div></div>
+        <button
+          onClick={() => {
+            displayIt ? setDisplayIt(false) : setDisplayIt(true);
+          }}
+        >
+          <i
+            className={
+              displayIt
+                ? "open fa-solid fa-chevron-down"
+                : "fa-solid fa-chevron-down"
+            }
+          ></i>
+        </button>
       </div>
-      <div className="data-container">
-        <p>
-          User :
-          <input type="text" defaultValue={user} />
-        </p>
-        <p>
-          Password :
-          <input type="text" defaultValue={pass} />
-        </p>
-      </div>
+      {displayIt && (
+        <div className="data-container">
+          <p>
+            User :
+            <input type="text" defaultValue={user} />
+          </p>
+          <p>
+            Password :
+            <input type="text" defaultValue={pass} />
+          </p>
+        </div>
+      )}
     </article>
   );
 };
