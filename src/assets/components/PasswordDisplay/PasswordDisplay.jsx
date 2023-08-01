9b5b6a4fb3ab17afd3a61c402d336796ecc83401
MdNData <nd.mirica@gmail.com>
2023-08-01 16:21:06 +0300

    update

--- src/assets/components/PasswordDisplay/PasswordDisplay.jsx
+++ src/assets/components/PasswordDisplay/PasswordDisplay.jsx
@@ -3,10 +3,24 @@ import { ErrorPopUp } from "../errorPopUp/ErrorPopUp";
 import { PlaceholderPassword } from "./PasswordPlaceholder/PlaceholderPassword";
 import { PasswordItem } from "./PasswordItem/PasswordItem";
 import { fetchEntries } from "../../data/GetAll";
+import { NewPassword } from "./NewPassword/NewPassword";
 
 export const PasswordDisplay = () => {
   //manage the entry data
   const [entryData, setEntryData] = useState(null);
+  const [classNewPass, setClassNewPass] = useState("newPasswordHidden");
+  const [addNewButton, setAddNewButton] = useState("+ Add New Entry");
+
+  //toggle new password field
+  const displayNewPassword = () => {
+    if (classNewPass == "newPasswordHidden") {
+      setClassNewPass("newPasswordVisible");
+      setAddNewButton("Never Mind");
+    } else {
+      setClassNewPass("newPasswordHidden");
+      setAddNewButton("+ Add New Entry");
+    }
+  };
 
   useEffect(() => {
     //get all Pass
@@ -27,6 +41,26 @@ export const PasswordDisplay = () => {
   return (
     //main container for the passwords fields
     <section className="password-display-container">
+      {/* BUTTONS */}
+      <div className="buttons-container-password">
+        <div className="button">
+          <div>
+            <span onClick={displayNewPassword}>{addNewButton}</span>
+          </div>
+        </div>
+        <div className="button">
+          <div>
+            <span>
+              <i className="fa-solid fa-magnifying-glass"></i>
+            </span>
+          </div>
+        </div>
+      </div>
+
+      {/* NEW PASSWORD FORM */}
+      <NewPassword className={classNewPass} />
+
+      {/* PASSWORDS LIST */}
       {entryData.map((entry) => {
         const { id } = entry;
         return <PasswordItem entry={entry} key={id} />;
