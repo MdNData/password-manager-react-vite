3f970e886c0b7fb4e9403d69922f547e0631ae5f
MdNData <nd.mirica@gmail.com>
2023-09-16 11:08:26 +0300

    global context

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 import "./assets/styles/index.css";
+import AppContext from "./GlobalContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.Fragment>
-    <App />
-  </React.Fragment>,
-)
+    <AppContext>
+      <App />
+    </AppContext>
+  </React.Fragment>
+);
